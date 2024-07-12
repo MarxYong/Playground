4beb77486655326dc19246294f24498b095c210c
Marcus <mjie.marcus@gmail.com>
2024-07-11 20:33:16 -0700

    simplified route

--- app/Livewire/LandingPage.php
+++ app/Livewire/LandingPage.php
@@ -7,10 +7,20 @@ use Livewire\Component;
 class LandingPage extends Component
 {
     public $count = 1;
+    public $todo = '';
+    public $todos = [
+        'no body cares',
+        'who else',
+    ];
 
     public function increment($by){
         // $this->count++;
         $this->count = $this->count + $by;
+    }
+
+    public function add(){
+        $this->todos[] = $this->todo;
+        $this->reset('todo');
     }
 
     public function render()
--- resources/views/livewire/landing-page.blade.php
+++ resources/views/livewire/landing-page.blade.php
@@ -5,4 +5,14 @@
 
     Count: {{ $count }}
     <button wire:click.throttle.1000ms="increment(2)">+</button>
+
+    <form wire:submit="add">
+        <input type="text" wire:model="todo">
+        <button type="submit">add</button>
+    </form>
+    <ul>
+        @foreach ($todos as $todo)
+        <li>{{ $todo }}</li>
+        @endforeach
+    </ul>
 </div>

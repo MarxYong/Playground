79b29dc3cadfb2e0cd653fb2bb00edeedbe70a48
Marcus <mjie.marcus@gmail.com>
2024-07-16 21:23:04 -0700

    abort changes

--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Livewire\LandingPage;
-use App\Livewire\ClassesManagement;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ClassesController;
-
-Route::get('/', LandingPage::class);
-Route::get('/', [ClassesController::class, 'index']);
